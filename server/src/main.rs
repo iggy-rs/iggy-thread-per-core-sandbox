5af452bc9229d5d99f8ef8871b4126911f8058e5
numinex <g.koszyk98@gmail.com>
2024-05-11 20:07:18 +0200

    Start working on sharding + impl select

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,9 @@
-use std::thread::available_parallelism;
+use std::{io::stdin, thread::available_parallelism};
+
+use monoio::{
+    io::{AsyncReadRentExt, Splitable},
+    net::{TcpListener, TcpStream},
+};
 
 #[cfg(target_os = "linux")]
 fn main() {
@@ -8,62 +13,30 @@ fn main() {
 
 #[cfg(target_os = "linux")]
 fn run() {
+    //use monoio::net::TcpListener;
     const ADDRESS: &str = "127.0.0.1:50000";
     let available_threads = available_parallelism().unwrap().get();
     println!("Available threads: {available_threads}");
 
     let mut threads = Vec::new();
-    use monoio::{
-        io::{AsyncReadRentExt, AsyncWriteRentExt, Splitable},
-        net::TcpListener,
-    };
-    for _ in 0..available_threads {
-        let thread = std::thread::spawn(|| {
+    for cpu in 0..available_threads {
+        let thread = std::thread::spawn(move || {
+            monoio::utils::bind_to_cpu_set(Some(cpu)).unwrap();
             let mut rt = monoio::RuntimeBuilder::<monoio::IoUringDriver>::new()
                 .enable_timer()
                 .build()
                 .unwrap();
             rt.block_on(async move {
+                /*
                 let listener = TcpListener::bind(ADDRESS)
                     .unwrap_or_else(|_| panic!("[Server] Unable to bind to {ADDRESS}"));
+                */
                 println!("[Server] Bind ready with address {ADDRESS}");
-                loop {
-                    match listener.accept().await {
-                        Ok((stream, _)) => {
-                            println!("[Server] Accepted connection");
-                            monoio::spawn(async move {
-                                let (mut reader, mut writer) = stream.into_split();
-                                loop {
-                                    let buf = vec![0; 10];
-                                    let (n, buf) = reader.read_exact(buf).await;
-                                    let n = n.unwrap();
-                                    println!(
-                                        "[Server {:?}] Read {} bytes data",
-                                        std::thread::current().id(),
-                                        n
-                                    );
-                                    if n == 0 {
-                                        break;
-                                    }
-                                    let (written, _) = writer.write_all(buf).await;
-                                    let written = written.unwrap();
-                                    println!(
-                                        "[Server {:?}] Written {} bytes data",
-                                        std::thread::current().id(),
-                                        written
-                                    );
-                                    if written != n {
-                                        println!("Failed to write all data");
-                                    }
-                                }
-                            });
-                        }
-                        Err(e) => {
-                            println!("[Server] Error accepting connection: {e}");
-                            continue;
-                        }
-                    }
+                monoio::select! {
+                    //res = listen(listener) => { res.unwrap()},
+                    res = console_input() => { res.unwrap()}
                 }
+                loop {}
             });
         });
         threads.push(thread);
@@ -72,4 +45,52 @@ fn run() {
     for thread in threads {
         thread.join().unwrap();
     }
+}
+
+async fn console_input() -> std::io::Result<()> {
+    let stdin = stdin();
+    let mut user_input = String::new();
+    loop {
+        stdin.read_line(&mut user_input)?;
+        print!("User input: {user_input}");
+        user_input.clear();
+    }
+}
+
+async fn listen(listener: TcpListener) -> std::io::Result<()> {
+    match listener.accept().await {
+        Ok((stream, _)) => {
+            println!("[Server] Accepted connection");
+            monoio::spawn(async move {
+                if let Err(e) = handle_connection(stream).await {
+                    println!("Error handling connection: {e}")
+                }
+            });
+        }
+        Err(e) => {
+            println!("[Server] Error accepting connection: {e}");
+            return Err(e);
+        }
+    }
+    Ok(())
+}
+
+async fn handle_connection(stream: TcpStream) -> Result<(), std::io::Error> {
+    let (mut reader, _) = stream.into_split();
+    let buf = Box::new([0u8; 4]);
+    let (n, buf) = reader.read_exact(buf).await;
+    let n = n?;
+    println!(
+        "[Server {:?}] Read {} bytes data",
+        std::thread::current().id(),
+        n
+    );
+
+    let shard_id = u32::from_le_bytes(*buf);
+    println!(
+        "[Server {:?}] Received shard id: {}",
+        std::thread::current().id(),
+        shard_id
+    );
+    Ok(())
 }
---
+++ server/src/shard.rs
@@ -0,0 +1,7 @@
+pub struct Shard {
+
+}
+
+pub struct ShardChannel {
+
+}
