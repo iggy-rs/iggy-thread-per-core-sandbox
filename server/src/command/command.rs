0d6a9632a4ccda92ac1a25889539c3714e377154
numinex <g.koszyk98@gmail.com>
2024-05-14 21:52:07 +0200

    Implement Tcp listener and partition dispatcher using shards

---
+++ server/src/command/command.rs
@@ -0,0 +1,28 @@
+use std::fmt::Display;
+
+pub enum Command {
+    CreatePartition(),
+    SendToPartition(Vec<u8>),
+    ReadFromPartition(),
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Command::CreatePartition() => write!(f, "create partition"),
+            Command::SendToPartition(_) => write!(f, "send to partition"),
+            Command::ReadFromPartition() => write!(f, "read from partition"),
+        }
+    }
+}
+
+impl From<u32> for Command {
+    fn from(command_id: u32) -> Self {
+        match command_id {
+            0 => Command::CreatePartition(),
+            1 => panic!("Not allowed"),
+            2 => Command::ReadFromPartition(),
+            _ => unreachable!("Invalid command id: {command_id}"),
+        }
+    }
+}
